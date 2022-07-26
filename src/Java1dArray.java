e5d79787e35522c1f20622ac3e2bbf645efce72d
Artur Lee <arturkml@gmail.com>
2022-07-26 12:27:57 -0300

    Java1DArray - incompleto

---
+++ src/Java1dArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Java1dArray {
+
+    public static boolean canWin(int leap, int[] game) {
+        return(game[leap] == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
+    }
+}
